b6631316c1dfdda16da7e1b204888025f5f6e1f6
iLikeGrape <135122587+iLikeGrape@users.noreply.github.com>
2024-12-17 16:35:23 +0000

    test 2

--- garden.js
+++ garden.js
@@ -1,14 +1,17 @@
-const cells = document.querySelectorAll(".cell");
-let cellId = {
+const cells = document.querySelectorAll(".cell"); // cells becomes array with data of each element with id "cell"
+
+let cellId = { //used for pushing data into planted array
     row : null,
     collumn : null,
 };
-let plantedArray = [];
+
+let plantedArray = []; //used to store data of which cells have plants
 
 
 function initializeGame(){
     cells.forEach(cell => cell.addEventListener("click", cellClicked))
 }
+
 function cellClicked(){
     let cellId = {
         row : this.getAttribute("cellRow"),
@@ -17,13 +20,18 @@ function cellClicked(){
     plantedArray.push(cellId)
 }
 function update(){
-    cells.forEach(updateCells(cell))
 }
-function updateCells(cell){
-    let cellId = {
-        row : cell.getAttribute("cellRow"),
-        collumn : cell.getAttribute("cellCollumn"),
+function updateCells(){
+    for(let i=0; i < cells.length; i++){
+        let cellId = cells[i] //seting cellId placeholder to each cell at a time
+
+        for(let i=0; i < plantedArray.length; i++){ //for each cell, checks every slot of planted array to look for a match
+            if(cellId == plantedArray[i]){
+                cell.textContent = "X"
+            }
+        }
     }
+
     for(let i=0; i < plantedArray.length; i++){
         if(cellId == plantedArray[i]){
             this.textContent = "X"
